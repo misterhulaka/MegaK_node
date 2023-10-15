a1cf272549ce05689cf0a6ffb04f549eac441dc7
misterhulaka <misterhulaka@gmail.com>
2023-10-15 18:31:03 +0200

    added dns node module

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const { log } = require('console');
 const { readFile } = require('fs');
 const { promisify } = require('util');
+const {lookup} = require('dns');
 
 console.log('Hello');
+// getFile();
 
 // const readFilePromised = promisify(readFile);
+function getFile(file) {
+	(async () => {
+		try {
+			const data = await promisify(readFile)('./index.js', 'utf-8');
+			console.log(data);
+		} catch (err) {
+			console.log('Oh no!', err);
+		}
+	})();
+}
 
-(async () => {
-	try {
-		const data = await promisify(readFile)('./index.js', 'utf-8');
-		console.log(data);
-	} catch (err) {
-		console.log('Oh no!', err);
-	}
-})();
+lookup('google.com', (err, address) => {
+	console.log(address);
+});
+
 
 //Promised fs
 // readFilePromised('./index.js','utf-8')
