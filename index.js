d2bad85355d9b022f11290eb9b21d9a705f13636
misterhulaka <misterhulaka@gmail.com>
2023-10-24 21:48:53 +0200

    added monitFiles.js -> fetch refactored

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const fetch = require('node-fetch');
 const cityName = process.argv[2];
 
 const processWeatherData = data => {
-	const foundData = data.find(stationData => {
-		if (stationData.stacja === cityName) {
-			// console.log(foundData)
-			return true;
-		} else {
-			return false;
-		}
-	});
-	console.log(foundData);
+	const foundData = data.find(stationData => stationData.stacja === cityName);
+	if (!foundData) {
+		console.log(`-> ${cityName} <- not found`);
+	}
+	const {
+		cisnienie: pressure,
+		wilgotnosc_wzgledna: humidity,
+		temperatura: temperature,
+	} = foundData;
+
+	const weatherInfo = `In ${cityName} there is ${temperature}°C, ${humidity}% of humidity and pressure of ${pressure} hPa `
+	console.log(weatherInfo);
 };
 
 fetch('https://danepubliczne.imgw.pl/api/data/synop/')
@@ -19,14 +22,14 @@ fetch('https://danepubliczne.imgw.pl/api/data/synop/')
 	.then(processWeatherData)
 	.catch(error => console.log('Error', error));
 
-	// fetch('https://danepubliczne.imgw.pl/api/data/synop/')
-	// .then(response => {
-	// 	console.log('Answer ->', response);
-	// 	return response.json();
-	// })
-	// // .then(response => response.json())
-	// .then(data => console.log(data))
-	// .catch(error => console.log('Error', error));
+// fetch('https://danepubliczne.imgw.pl/api/data/synop/')
+// .then(response => {
+// 	console.log('Answer ->', response);
+// 	return response.json();
+// })
+// // .then(response => response.json())
+// .then(data => console.log(data))
+// .catch(error => console.log('Error', error));
 
 // fetch('https://github.com/')
 //     .then(res => res.text())
