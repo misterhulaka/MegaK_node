16d99e80d0bdf8621bfa4c00f60c3a84ab77c03c
misterhulaka <misterhulaka@gmail.com>
2023-10-24 21:14:10 +0200

    added monitFiles.js -> fetch successful

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const fetch = require('node-fetch');
 
+const cityName = process.argv[2];
+
+const processWeatherData = data => {
+	const foundData = data.find(stationData => {
+		if (stationData.stacja === cityName) {
+			// console.log(foundData)
+			return true;
+		} else {
+			return false;
+		}
+	});
+	console.log(foundData);
+};
+
 fetch('https://danepubliczne.imgw.pl/api/data/synop/')
-	.then(response => {
-		console.log('Answer:');
-		console.dir(response);
-		console.log('Answer content:');
-		console.log(response.json())
-	})
-	.then(html => console.log(html))
+	.then(response => response.json())
+	.then(processWeatherData)
 	.catch(error => console.log('Error', error));
+
+	// fetch('https://danepubliczne.imgw.pl/api/data/synop/')
+	// .then(response => {
+	// 	console.log('Answer ->', response);
+	// 	return response.json();
+	// })
+	// // .then(response => response.json())
+	// .then(data => console.log(data))
+	// .catch(error => console.log('Error', error));
 
 // fetch('https://github.com/')
 //     .then(res => res.text())
