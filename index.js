d92328308b80ac5eadd59737f91f888a3050742c
misterhulaka <misterhulaka@gmail.com>
2023-10-24 22:18:35 +0200

    modified index.js -> save weather to file

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const fetch = require('node-fetch');
+const { appendFile } = require('fs').promises;
+const { normalize, resolve } = require('path');
 
 const cityName = process.argv[2];
 
-const processWeatherData = data => {
+function safeJoin(base, target) {
+	const targetPath = '.' + normalize('/' + target);
+	return resolve(base, targetPath);
+}
+
+const getDataFileName = city => safeJoin(`./data/weather/`,`${city}.txt`);
+
+const processWeatherData = async data => {
 	const foundData = data.find(stationData => stationData.stacja === cityName);
+
 	if (!foundData) {
 		console.log(`-> ${cityName} <- not found`);
+		return;
 	}
+	
 	const {
 		cisnienie: pressure,
 		wilgotnosc_wzgledna: humidity,
 		temperatura: temperature,
 	} = foundData;
 
-	const weatherInfo = `In ${cityName} there is ${temperature}°C, ${humidity}% of humidity and pressure of ${pressure} hPa `
+	const weatherInfo = `-> In ${cityName} there is ${temperature}°C, ${humidity}% of humidity and pressure of ${pressure} hPa `
 	console.log(weatherInfo);
+
+	const dateTimeString = new Date().toLocaleString();
+
+	await appendFile(getDataFileName(cityName), `${dateTimeString}\n${weatherInfo}\n`);
 };
 
 fetch('https://danepubliczne.imgw.pl/api/data/synop/')
