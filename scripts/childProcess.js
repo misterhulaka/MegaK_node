77d72c222339a658dd4ca4cfe001660ff4051401
misterhulaka <misterhulaka@gmail.com>
2023-10-28 14:29:22 +0200

    added childProcess.js -> iife

--- scripts/childProcess.js
+++ scripts/childProcess.js
@@ -1,7 +1,36 @@
-const {promisify} = require('util');
+const { log } = require('console');
+const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
+function testPingIIFE() {
 
-exec('dir')
-.then(({stdout, stderr}) => {
-	console.log(stdout);
-})
+	(async () => {
+		try {
+			const { stdout } = await exec('ping 8.8.hjghj8.8');
+			console.log(stdout);
+		} catch (error) {
+			console.log(error.stdout);
+		}
+	}
+
+	)();
+}
+
+function exeCommand(command) {
+	exec(command, (error, stdout, stderr) => {
+		console.log({ error, stdout, stderr })
+	});
+}
+
+function exeComm(command) {
+	exec(command, (error, stdout, stderr) => {
+		if (error) {
+			console.error('Stack error occurred ->', error);
+		} else if (stderr) {
+			console.error('App error occurred', stderr);
+		} else {
+			console.log('Program has finished', stdout);
+		}
+	});
+}
+
+exeCommand(process.argv[2]);
