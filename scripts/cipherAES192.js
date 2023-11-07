10b1993e8db3f08748bf8ff7c5322dcfca250c7d
misterhulaka <misterhulaka@gmail.com>
2023-11-08 00:32:13 +0100

    modified encryptionFileSolution.js -> minor tweaks, encryptionFileSolution.js -> repaired error in function

--- scripts/cipherAES192.js
+++ scripts/cipherAES192.js
@@ -1,10 +1,8 @@
 const { promisify } = require('util');
 const scrypt = promisify(require('crypto').scrypt);
-// const createHmac = require('crypto');
 const randomBytes = promisify(require('crypto').randomBytes);
 const { createCipheriv, createDecipheriv, createHmac } = require('crypto');
-const { SALT, HASH_SALT } = require('../data/constants');
-const { error } = require('console');
+
 
 
 
@@ -12,7 +10,6 @@ async function encryptText(text, password, salt) {
 	const algorithm = 'aes-192-cbc';
 	const key = await scrypt(password, salt, 24);
 	const iv = await randomBytes(16);
-
 
 	const cipher = createCipheriv(algorithm, key, iv);
 	let encrypted = cipher.update(text, 'utf8', 'hex');
@@ -31,13 +28,6 @@ async function decryptText(encryptedText, password, salt, ivHex) {
 	const decipher = createDecipheriv(algorithm, key, iv);
 	let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
 	decrypted += decipher.final('utf8');
-	const decryptedHash = hash(decrypted, HASH_SALT); 
-	console.log(decryptedHash);
-	if (encryptText.textHash === decryptedHash) {
-		console.log('Plik zgodny!');
-	} else {
-		console.error(error);
-	}
 	return decrypted;
 };
 
--- scripts/exercises/encryptionFileSolution.js
+++ scripts/exercises/encryptionFileSolution.js
@@ -2,8 +2,8 @@
 // const { promisify } = require('util');
 const { readFile, writeFile } = require('fs').promises;
 const { encryptText, decryptText, hash } = require('../cipherAES192');
-const {createHmac} = require('crypto')
-const {ENCRYPTION_SAL, HASH_SALT} = require('../../data/constants');
+const { createHmac } = require('crypto')
+const { ENCRYPTION_SALT, HASH_SALT } = require('../../data/constants');
 
 // const SALT = 'djfnisjdnfojdsnofnsdognosnfoieonfosdofnsdpgspgmpsmgpsdpgmpsdmgpmpdgmpslmgpmspgmpsm';
 
@@ -20,19 +20,27 @@ if (methodName === 'decryptFile') {
 
 async function encryptFile(fileName, pwd) {
 	const content = await readFile(fileName, 'utf8');
-	const contentHash = hash(content, HASH_SALT );
-	console.log(contentHash);
-	const encrypted = await encryptText(content, pwd, ENCRYPTION_SAL);
+	const contentHash = hash(content, HASH_SALT);
+	console.log("Hash:", contentHash);
+	const encrypted = await encryptText(content, pwd, ENCRYPTION_SALT);
 	encrypted.hash = contentHash;
 	await writeFile(fileName, JSON.stringify(encrypted), 'utf8');
-	console.log('Done.');
+	console.log("> ENCRYPTING FILE DONE <");
 }
 
 async function decryptFile(fileName, pwd) {
 
 	const json = await readFile(fileName, 'utf8');
 	const encrypted = JSON.parse(json);
-	const decrypted = await decryptText(encrypted.encrypted, pwd, ENCRYPTION_SAL, encrypted.iv);
-	console.log(decrypted);
-	await writeFile(fileName, decrypted, 'utf8');
+	const decrypted = await decryptText(encrypted.encrypted, pwd, ENCRYPTION_SALT, encrypted.iv);
+	const decryptedContentHash = hash(decrypted, HASH_SALT);
+	console.log("Hash:", decryptedContentHash);
+	if (encrypted.hash === decryptedContentHash) {
+		await writeFile(fileName, decrypted, 'utf8');
+		console.log("> FILE IS ORIGINAL <");
+		console.log("> FILE CONTENT <\n" + decrypted);
+		console.log("> DECRYPTING FILE DONE <");
+	} else {
+		console.error("> FILE IS NOT ORIGINAL <");
+	}
 }
