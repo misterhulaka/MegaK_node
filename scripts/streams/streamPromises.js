05d566cab74dbd886121bd97fb7b487695fbe65c
misterhulaka <misterhulaka@gmail.com>
2023-11-09 02:59:49 +0100

    added streamPromises.js modified for 3 method

--- scripts/streams/streamPromises.js
+++ scripts/streams/streamPromises.js
@@ -1,18 +1,61 @@
 const { createReadStream, createWriteStream } = require('fs');
 const { pipeline } = require('stream').promises;
 
-const openFileStream = createReadStream('./data/streams/bigText.txt');
-// console.log(openFileStream);
-
-const writeFileStream = createWriteStream('./data/streams/bigTextCopied.txt');
-
 //stream.promises.pipline()
 
-(async () => {
+const [, , methodName, file, path] = process.argv;
 
+console.log({ methodName, file, path });
+
+if (methodName === undefined) {
+	console.log('Choose method name:\n',
+		'> streamCopyFile file path\n',
+		'> streamPipeCopyFile file path\n',
+		'> streamManualCopyFile file path\n');
+	return;
+}
+if (methodName === 'streamCopyFile') {
+	streamCopyFile(file, path);
+}
+if (methodName === 'streamPipeCopyFile') {
+	streamPipeCopyFile(file, path);
+}
+if (methodName === 'streamManualCopyFile') {
+	streamManualCopyFile(file, path);
+}
+
+async function streamCopyFile(targetFile, targetPath) {
+	console.time('streamFile');
 	await pipeline(
-		openFileStream,
-		writeFileStream
+		createReadStream(targetFile),
+		createWriteStream(targetPath)
 	);
 	console.log('Done.');
-})();
+	console.timeEnd('streamFile');
+}
+
+function streamPipeCopyFile(targetFile, targetPath) {
+	console.time('streamFile');
+	const readStream = createReadStream(targetFile);
+	const writeStream = createWriteStream(targetPath);
+
+	readStream.pipe(writeStream);
+	readStream.on('end', () => {
+		console.log('Ready');
+		console.timeEnd('streamFile');
+	});
+}
+
+function streamManualCopyFile(targetFile, targetPath) {
+	console.time('streamFile');
+	const readStream = createReadStream(targetFile);
+	const writeStream = createWriteStream(targetPath);
+
+	readStream.on('data', (data) => writeStream.write(data));
+	readStream.on('end', () => {
+		writeStream.close();
+		console.log('Done.');
+		console.timeEnd('streamFile');
+	});
+
+}
