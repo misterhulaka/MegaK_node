54d3212f47802b149991d00f50da39de0e24ccaa
misterhulaka <misterhulaka@gmail.com>
2023-11-12 13:23:37 +0100

    modified scripts/http -> set up of working local webserver

--- scripts/http/index.js
+++ scripts/http/index.js
@@ -1,8 +1,30 @@
 const { createServer } = require('http');
+const { readFile } = require('fs').promises;
 
-const server = createServer((req, res) => {
-	res.writeHead(200, {'Content-type' : 'text/html'});
-	res.end('<h1>hello megaK</h1>');
-}).listen(3000, '127.0.0.1');
+const server = createServer();
+
+server.on('request', async (req, res) => {
+	console.log(req.url);
+	if (req.url === '/' && req.method === 'GET') {
+
+		console.log(req.headers);
+
+		const html = await readFile('./index.html', 'utf8');
+
+		res.writeHead(200, {
+			'Content-type': 'text/html',
+			'X-My-Header': 'Hello',
+		});
+		res.end(html);
+	} else {
+		console.log(req.url);
+		res.writeHead(404, {
+			'Content-type': 'text/html',
+		});
+		res.end('<h1>Not found.</h1>');
+	}
+})
+
+server.listen(3000, 'localhost');
 
 // console.log(server);
